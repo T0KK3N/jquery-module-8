d7043e96060ae3bffb92d2e95d5233918ca7ab6a
T0KK3N <daniel.mora.ortiz@udla.edu.ec>
2021-06-08 20:51:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jquery-module-8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jquery-module-8)
---
+++ index.js
@@ -0,0 +1,31 @@
+import './style.css';
+
+window.listaCompras = [];
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    var toAdd = $('input[name=itemCompras]').val();
+    $('ol').append('<li>' + toAdd + '</li>');
+    $('input').val('');
+    window.listaCompras.push(toAdd);
+    localStorage.setItem('listaCompras', JSON.stringify(window.listaCompras));
+  });
+
+  $('input[name=itemCompras]').keyup(function(event) {
+    if (event.keyCode == 13) {
+      $('#button').click();
+    }
+  });
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+  });
+
+  $(document).on('dblclick', 'li', function() {
+    $(this)
+      .toggleClass('strike')
+      .fadeOut(500, function() {
+        $(this).remove();
+      });
+  });
+});
